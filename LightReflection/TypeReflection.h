15ddb480dcacf16b8e7c7c4268836d3341ea9738
ViperCC <461015895@qq.com>
2017-06-14 22:32:02 +0800

    update

--- LightReflection/TypeReflection.h
+++ LightReflection/TypeReflection.h
@@ -19,6 +19,12 @@ unsigned int FuncToAddr(T(*func)(Args...))
 	return fau.addr;
 }
 
+template<class T, class... Args>
+T* ClassCreateFunc(Args... args)
+{
+	return new T(args...);
+}
+
 template<class T>
 void ClassReleaseFunc(void* p)
 {
@@ -27,17 +33,13 @@ void ClassReleaseFunc(void* p)
 
 #define REF_CREATOR(params_count, class_type, ...) \
 	template<class... Args> \
-	void* Create##class_type##params_count(Args... args) \
-	{ \
-		return new class_type(args...); \
-	} \
 	class Create##class_type##params_count##ForFuncAddr { \
 		public: Create##class_type##params_count##ForFuncAddr() {  \
 					Reflection* ref = Reflection::CreateInstance(); \
-					ref->RegisterConstructor(#class_type, FuncToAddr<void*, __VA_ARGS__>(Create##class_type##params_count), params_count); \
+					ref->RegisterConstructor(#class_type, FuncToAddr<class_type*, Args...>(ClassCreateFunc<class_type, Args...>), params_count); \
 					ref->RegisterDestructor(#class_type, FuncToAddr<void, void*>(ClassReleaseFunc<class_type>)); \
 		} \
 	}; \
-	Create##class_type##params_count##ForFuncAddr* Create##class_type##params_count##ForFuncAddr##Object = new Create##class_type##params_count##ForFuncAddr();
+	Create##class_type##params_count##ForFuncAddr<__VA_ARGS__>* Create##class_type##params_count##ForFuncAddr##Object = new Create##class_type##params_count##ForFuncAddr<__VA_ARGS__>();
 
 #endif
